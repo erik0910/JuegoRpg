17b363042ea1634bb30c61f25859bb192ee38da1
erik0910 <erikmartinez0910@gmail.com>
2021-04-02 12:21:23 +0200

    implementacion rama master y cambios de objetos

---
+++ Rpg/src/main/java/Prueba/Animacion.java
@@ -0,0 +1,31 @@
+package Prueba;
+
+import java.awt.image.BufferedImage;
+
+public class Animacion {
+	private BufferedImage[] frames;
+	private int frameActual;
+	private long tiempoInicial,delay;
+	
+	public void setAnimacion(BufferedImage[] frames) { //Mandas una imagen y la cambias a la animación privada del objeto que la ha llamado.
+		this.frames = frames; //Cambiar las imagenes
+		frameActual = 0;
+		tiempoInicial = System.nanoTime();
+	}
+	
+	public void setDelay(long d) { delay = d; }
+	
+	public void update() {
+		if(delay == -1) return;
+		long elapsed = (System.nanoTime() - tiempoInicial) / 1000000;
+		if(elapsed > delay) {
+			frameActual++;
+			tiempoInicial = System.nanoTime();
+		}
+		if(frameActual == frames.length) frameActual = 0;
+	}
+	
+	public int getFrame() { return frameActual; }
+	public BufferedImage getImage() { return frames[frameActual]; }
+
+}
---
+++ Rpg/src/main/java/Prueba/Armas.java
@@ -0,0 +1,51 @@
+package Prueba;
+
+
+public class Armas {
+	private int danyo,speed,knockback,frames,x,y,alto,ancho;
+	private String nombre;
+	
+	public Armas(int danyo, int speed, int knockback, int frames, int x, int y, int alto, int ancho, String nombre) {
+		super();
+		this.danyo = danyo;
+		this.speed = speed;
+		this.knockback = knockback;
+		this.frames = frames;
+		this.x = x;
+		this.y = y;
+		this.alto = alto;
+		this.ancho = ancho;
+		this.nombre = nombre;
+	}
+
+	public boolean ataque(double xe, double ye, int altoe, int anchoe) {
+		boolean ataque = false;
+		if(ye >= y && (ye <= y+alto)) { //y >= 200 y y <= 250
+			if(xe >= x && xe <= x+ancho) {// x >= 100 y x <= 200
+				System.out.println("Ataque");
+				ataque = true;
+			}
+		}
+		return ataque;
+	}
+	
+	public int getDanyo() {return danyo;}
+	public void setDanyo(int danyo) {this.danyo = danyo;}
+	public int getSpeed() {return speed;}
+	public void setSpeed(int speed) {this.speed = speed;}
+	public int getKnockback() {return knockback;}
+	public void setKnockback(int knockback) {this.knockback = knockback;}
+	public int getFrames() {return frames;}
+	public void setFrames(int frames) {this.frames = frames;}
+	public int getX() {return x;}
+	public void setX(int x) {this.x = x;}
+	public int getY() {return y;}
+	public void setY(int y) {this.y = y;}
+	public int getAlto() {return alto;}
+	public void setAlto(int alto) {this.alto = alto;}
+	public int getAncho() {return ancho;}
+	public void setAncho(int ancho) {this.ancho = ancho;}
+	public String getNombre() {return nombre;}
+	public void setNombre(String nombre) {this.nombre = nombre;}
+
+}
