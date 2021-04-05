f2b845c1248cb994f926bd1a634b7f8aa1f062e6
David <33113652+DavidLopezLoureiro@users.noreply.github.com>
2021-04-05 22:22:16 +0200

    Arreglo de las peleas y añadidos algunos elementos extra al mapa para hacerlo más vivo

--- Rpg/src/Code/Array.java
+++ Rpg/src/Code/Array.java
@@ -13,16 +13,15 @@ import javax.swing.JPanel;
 import javax.swing.border.EmptyBorder;
 
 import Clases.Tiles;
-import Clases.prueba;
 
 public class Array extends JFrame implements KeyListener {
 
 	static Tiles[][] mundo = new Tiles[50][50];
-	static Tiles suelo = new Tiles("grass", true, true, true, true, "e");
-	static Tiles rio = new Tiles("river", false, false, false, false, "e");
-	static Tiles puente = new Tiles("bridg", true, true, true, true, "e");
-	static Tiles parez = new Tiles("walls", false, false, false, false, "e");
-	static Tiles pelea = new Tiles("fight", true, true, true, true, "e");
+	static Tiles suelo = new Tiles("grass", true, true, true, true);
+	static Tiles rio = new Tiles("river", false, false, false, false);
+	static Tiles puente = new Tiles("bridg", true, true, true, true);
+	static Tiles pared = new Tiles("walls", false, false, false, false);
+	static Tiles pelea = new Tiles("fight", true, true, true, true);
 	static int x = 5;
 	static int y = 5;
 	static int x_ant;
@@ -66,43 +65,145 @@ public class Array extends JFrame implements KeyListener {
 
 				if (i == 0) {
 
-					mundo[i][u] = parez;
+					mundo[i][u] = pared;
 
 				}
 				if (i == 49) {
 
-					mundo[i][u] = parez;
+					mundo[i][u] = pared;
 
 				}
 				if (u == 0) {
 
-					mundo[i][u] = parez;
+					mundo[i][u] = pared;
 
 				}
 				if (u == 49) {
 
-					mundo[i][u] = parez;
+					mundo[i][u] = pared;
 
 				}
 
 			}
 		}
 
-		for (int y = 0; y < 50; y++) {
+		for (int i = 25; i < 30; i++) {
+			for (int u = 25; u < 30; u++) {
+
+				mundo[i][u] = pared;
+
+			}
+		}
+
+		mundo[27][25] = suelo;
+		mundo[27][26] = suelo;
+		mundo[27][28] = suelo;
+
+		mundo[26][26] = suelo;
+		mundo[28][26] = suelo;
+
+		mundo[26][27] = suelo;
+		mundo[28][27] = suelo;
+
+		mundo[26][28] = suelo;
+		mundo[28][28] = suelo;
+
+		mundo[27][27] = pelea;
+
+		// Vertical
+		for (int y = 1; y < 49; y++) {
 
 			mundo[y][5] = rio;
 			mundo[y][4] = rio;
 		}
 
+		// Horizontal
+		for (int y = 1; y < 49; y++) {
+
+			mundo[45][y] = rio;
+			mundo[44][y] = rio;
+		}
+
+		// Horizontales
+		mundo[45][10] = puente;
+		mundo[44][10] = puente;
+		mundo[45][11] = puente;
+		mundo[44][11] = puente;
+
+		mundo[45][16] = puente;
+		mundo[44][16] = puente;
+		mundo[45][15] = puente;
+		mundo[44][15] = puente;
+
+		mundo[45][20] = puente;
+		mundo[44][20] = puente;
+		mundo[45][21] = puente;
+		mundo[44][21] = puente;
+
+		mundo[45][27] = puente;
+		mundo[44][27] = puente;
+		mundo[45][26] = puente;
+		mundo[44][26] = puente;
+
+		mundo[45][35] = puente;
+		mundo[44][35] = puente;
+		mundo[45][34] = puente;
+		mundo[44][34] = puente;
+
+		mundo[45][45] = puente;
+		mundo[44][45] = puente;
+		mundo[45][44] = puente;
+		mundo[44][44] = puente;
+
+		// Cruce
+		mundo[45][4] = puente;
+		mundo[44][4] = puente;
+		mundo[43][4] = puente;
+		mundo[43][5] = puente;
+		mundo[45][5] = puente;
+		mundo[45][6] = puente;
+		mundo[44][6] = puente;
+		mundo[45][3] = puente;
+		mundo[44][3] = puente;
+		mundo[44][5] = puente;
+		mundo[46][4] = puente;
+		mundo[46][5] = puente;
+
+		// Verticales
 		mundo[6][4] = puente;
 		mundo[6][5] = puente;
 		mundo[5][4] = puente;
 		mundo[5][5] = puente;
 
+		mundo[16][4] = puente;
+		mundo[16][5] = puente;
+		mundo[15][4] = puente;
+		mundo[15][5] = puente;
+
+		mundo[27][4] = puente;
+		mundo[27][5] = puente;
+		mundo[26][4] = puente;
+		mundo[26][5] = puente;
+
+		mundo[35][4] = puente;
+		mundo[35][5] = puente;
+		mundo[34][4] = puente;
+		mundo[34][5] = puente;
+
+		// Demos
 		mundo[6][8] = pelea;
 		mundo[8][3] = pelea;
 		mundo[2][2] = pelea;
 		mundo[2][7] = pelea;
+
+		// Pelas de boss y secreta
+		mundo[48][1] = pelea;
+		mundo[47][2] = pared;
+
+		for (int y = 7; y < 48; y++) {
+
+			mundo[47][y] = pared;
+		}
 
 		dibujado();
 
@@ -116,10 +217,8 @@ public class Array extends JFrame implements KeyListener {
 
 			JOptionPane.showMessageDialog(null, "Pelea");
 
-			x = x_ant;
-			y = y_ant;
-
 		}
+		repaint();
 
 		// Visual
 		this.getContentPane().removeAll();
@@ -141,34 +240,30 @@ public class Array extends JFrame implements KeyListener {
 
 				if (mundo[i + x_dib][u + y_dib].getCode().equals("grass")) {
 
-					mapa.setIcon(new ImageIcon(prueba.class.getResource("/Imagenes/hierba.png")));
+					mapa.setIcon(new ImageIcon(Array.class.getResource("/Imagenes/hierba.png")));
 
 				} else if (mundo[i + x_dib][u + y_dib].getCode().equals("walls")) {
 
 					if ((u == 0 || u == 9) && (i != 0 && i != 9)) {
-						mapa.setIcon(new ImageIcon(prueba.class.getResource("/Imagenes/paredgirada.png")));
+						mapa.setIcon(new ImageIcon(Array.class.getResource("/Imagenes/paredgirada.png")));
 
 					} else {
 
-						mapa.setIcon(new ImageIcon(prueba.class.getResource("/Imagenes/pared.png")));
+						mapa.setIcon(new ImageIcon(Array.class.getResource("/Imagenes/pared.png")));
 					}
 
 				} else if (mundo[i + x_dib][u + y_dib].getCode().equals("river")) {
 
-					// mapa.setIcon(new
-					// ImageIcon("C:\\Users\\david\\OneDrive\\Escritorio\\UNI\\Proceso de
-					// software\\water.gif"));
-
-					mapa.setIcon(new ImageIcon(prueba.class.getResource("/Imagenes/Agua.png")));
+					mapa.setIcon(new ImageIcon(Array.class.getResource("/Imagenes/Agua.png")));
 
 				} else if (mundo[i + x_dib][u + y_dib].getCode().equals("bridg")) {
 
-					mapa.setIcon(new ImageIcon(prueba.class.getResource("/Imagenes/bridge.png")));
+					mapa.setIcon(new ImageIcon(Array.class.getResource("/Imagenes/bridge.png")));
 
 				} else if (mundo[i + x_dib][u + y_dib].getCode().equals("fight")) {
 
-					mapa.setIcon(new ImageIcon(prueba.class.getResource("/Imagenes/hierba.png")));
-					pelea.setIcon(new ImageIcon(prueba.class.getResource("/Imagenes/calabera.png")));
+					mapa.setIcon(new ImageIcon(Array.class.getResource("/Imagenes/hierba.png")));
+					pelea.setIcon(new ImageIcon(Array.class.getResource("/Imagenes/calabera.png")));
 
 				} else {
 
@@ -186,19 +281,19 @@ public class Array extends JFrame implements KeyListener {
 					// Arquero
 					if (direcccion.equals("UP")) {
 
-						perso.setIcon(new ImageIcon(prueba.class.getResource("/Imagenes/arquero_detras_sf.png")));
+						perso.setIcon(new ImageIcon(Array.class.getResource("/Imagenes/arquero_detras_sf.png")));
 
 					} else if (direcccion.equals("DOWN")) {
 
-						perso.setIcon(new ImageIcon(prueba.class.getResource("/Imagenes/arquero_delante_sf.png")));
+						perso.setIcon(new ImageIcon(Array.class.getResource("/Imagenes/arquero_delante_sf.png")));
 
 					} else if (direcccion.equals("RIGHT")) {
 
-						perso.setIcon(new ImageIcon(prueba.class.getResource("/Imagenes/arquero.png")));
+						perso.setIcon(new ImageIcon(Array.class.getResource("/Imagenes/arquero.png")));
 
 					} else if (direcccion.equals("LEFT")) {
 
-						perso.setIcon(new ImageIcon(prueba.class.getResource("/Imagenes/arquero_girado_sf.png")));
+						perso.setIcon(new ImageIcon(Array.class.getResource("/Imagenes/arquero_girado_sf.png")));
 
 					}
 
@@ -215,24 +310,33 @@ public class Array extends JFrame implements KeyListener {
 			}
 		}
 
-		/**
-		 * // Consola System.out.println(); System.out.println(); System.out.println();
-		 * System.out.println();
-		 * 
-		 * for (int i = 0; i < 50; i++) { System.out.println(); for (int u = 0; u < 50;
-		 * u++) {
-		 * 
-		 * if (i == x && u == y) {
-		 * 
-		 * System.out.print("perso"); System.out.print(" ");
-		 * 
-		 * } else { System.out.print(mundo[i][u].getCode()); System.out.print(" "); }
-		 * 
-		 * } }
-		 * 
-		 * System.out.println(); System.out.println(); System.out.println();
-		 * System.out.println(); System.out.println(); System.out.println();
-		 **/
+		// Consola System.out.println(); System.out.println(); System.out.println();
+		System.out.println();
+
+		for (int i = 0; i < 50; i++) {
+			System.out.println();
+			for (int u = 0; u < 50; u++) {
+
+				if (i == x && u == y) {
+
+					System.out.print("perso");
+					System.out.print(" ");
+
+				} else {
+					System.out.print(mundo[i][u].getCode());
+					System.out.print(" ");
+				}
+
+			}
+		}
+
+		System.out.println();
+		System.out.println();
+		System.out.println();
+		System.out.println();
+		System.out.println();
+		System.out.println();
+
 	}
 
 	public void keyPressed(KeyEvent e) {
@@ -245,8 +349,6 @@ public class Array extends JFrame implements KeyListener {
 
 				if (mundo[x + 1][y].isDown()) {
 
-					x_ant = x;
-					y_ant = y;
 					x = x + 1;
 					if (x > 5 && x < 46) {
 						x_dib = x_dib + 1;
@@ -262,8 +364,6 @@ public class Array extends JFrame implements KeyListener {
 
 				if (mundo[x][y - 1].isLeft()) {
 
-					x_ant = x;
-					y_ant = y;
 					y = y - 1;
 					if (y > 4 && y < 45) {
 						y_dib = y_dib - 1;
@@ -279,8 +379,6 @@ public class Array extends JFrame implements KeyListener {
 
 				if (mundo[x][y + 1].isRight()) {
 
-					x_ant = x;
-					y_ant = y;
 					y = y + 1;
 					if (y > 5 && y < 46) {
 						y_dib = y_dib + 1;
@@ -298,8 +396,6 @@ public class Array extends JFrame implements KeyListener {
 
 				if (mundo[x - 1][y].isUp()) {
 
-					x_ant = x;
-					y_ant = y;
 					x = x - 1;
 					if (x < 45 && x > 4) {
 						x_dib = x_dib - 1;
