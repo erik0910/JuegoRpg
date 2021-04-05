a1fe37c204088ba7cac4ea14241ef7fefbe8e695
David <33113652+DavidLopezLoureiro@users.noreply.github.com>
2021-04-05 21:08:54 +0200

    Arreglo de la cámara en movimiento

--- Rpg/src/Code/Array.java
+++ Rpg/src/Code/Array.java
@@ -3,9 +3,6 @@ package Code;
 import java.awt.event.KeyEvent;
 import java.awt.Color;
 import java.awt.EventQueue;
-//import java.awt.Graphics;
-//import java.awt.Graphics2D;
-//import java.awt.Polygon;
 import java.awt.event.KeyListener;
 
 import javax.swing.ImageIcon;
@@ -20,17 +17,19 @@ import Clases.prueba;
 
 public class Array extends JFrame implements KeyListener {
 
-	static Tiles[][] map = new Tiles[10][10];
+	static Tiles[][] mundo = new Tiles[50][50];
 	static Tiles suelo = new Tiles("grass", true, true, true, true, "e");
 	static Tiles rio = new Tiles("river", false, false, false, false, "e");
 	static Tiles puente = new Tiles("bridg", true, true, true, true, "e");
 	static Tiles parez = new Tiles("walls", false, false, false, false, "e");
 	static Tiles pelea = new Tiles("fight", true, true, true, true, "e");
 	static int x = 5;
-	static int y = 1;
+	static int y = 5;
 	static int x_ant;
 	static int y_ant;
-	
+	static int x_dib = 0;
+	static int y_dib = 0;
+	static String direcccion = "UP";
 
 	private static final long serialVersionUID = 1L;
 
@@ -53,7 +52,6 @@ public class Array extends JFrame implements KeyListener {
 		addKeyListener(this);
 		setResizable(false);
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		// setBounds(100, 100, 1200, 589);
 		setBounds(100, 100, 700, 700);
 		contentPane = new JPanel();
 		contentPane.setBackground(Color.WHITE);
@@ -61,176 +59,63 @@ public class Array extends JFrame implements KeyListener {
 		setContentPane(contentPane);
 		contentPane.setLayout(null);
 
-		for (int i = 0; i < 10; i++) {
-			for (int u = 0; u < 10; u++) {
+		for (int i = 0; i < 50; i++) {
+			for (int u = 0; u < 50; u++) {
 
-				map[i][u] = suelo;
+				mundo[i][u] = suelo;
 
-			}
-		}
+				if (i == 0) {
 
-		for (int p = 0; p < 10; p++) {
+					mundo[i][u] = parez;
 
-			map[0][p] = parez;
-			map[9][p] = parez;
-			map[p][0] = parez;
-			map[p][9] = parez;
+				}
+				if (i == 49) {
 
-		}
+					mundo[i][u] = parez;
 
-		for (int y = 0; y < 10; y++) {
+				}
+				if (u == 0) {
 
-			map[y][5] = rio;
-			map[y][4] = rio;
-		}
+					mundo[i][u] = parez;
 
-		map[6][4] = puente;
-		map[6][5] = puente;
-		map[5][4] = puente;
-		map[5][5] = puente;
-		;
+				}
+				if (u == 49) {
 
-		map[6][8] = pelea;
-		map[8][3] = pelea;
-		map[2][2] = pelea;
-		map[2][7] = pelea;
+					mundo[i][u] = parez;
 
-		// Consola
-		for (int i = 0; i < 10; i++) {
-			System.out.println();
-			for (int u = 0; u < 10; u++) {
-
-				if (i == x && u == y) {
-
-					System.out.print("perso");
-					System.out.print("  ");
-
-				} else {
-					System.out.print(map[i][u].getCode());
-					System.out.print("  ");
 				}
 
 			}
 		}
 
+		for (int y = 0; y < 50; y++) {
+
+			mundo[y][5] = rio;
+			mundo[y][4] = rio;
+		}
+
+		mundo[6][4] = puente;
+		mundo[6][5] = puente;
+		mundo[5][4] = puente;
+		mundo[5][5] = puente;
+
+		mundo[6][8] = pelea;
+		mundo[8][3] = pelea;
+		mundo[2][2] = pelea;
+		mundo[2][7] = pelea;
+
 		dibujado();
 
 	}
-
-	/**
-	 * public void paint(Graphics g) { super.paint(g); int count = 0; Graphics2D g2
-	 * = (Graphics2D) g; // .drawLine(int x1, int y1, int x2, int y2)
-	 * 
-	 * for (int i = 0; i < 10; i++) {
-	 * 
-	 * for (int u = 0; u < 10; u++) {
-	 * 
-	 * if (i == x && u == y) {
-	 * 
-	 * Polygon polygon = new Polygon(); polygon.addPoint((50 + (145 * u + count)) /
-	 * 2, (-50 + (100 * (10 - i))) / 2); polygon.addPoint((195 + (145 * u + count))
-	 * / 2, (-50 + (100 * (10 - i))) / 2);
-	 * 
-	 * polygon.addPoint((95 + (145 * u + count)) / 2, (50 + (100 * (10 - i))) / 2);
-	 * polygon.addPoint((-50 + (145 * u + count)) / 2, (50 + (100 * (10 - i))) / 2);
-	 * 
-	 * g2.setColor(Color.RED); g2.fillPolygon(polygon);
-	 * 
-	 * } else if (map[i][u].getCode().equals("grass")) {
-	 * 
-	 * Polygon polygon = new Polygon(); polygon.addPoint((50 + (145 * u + count)) /
-	 * 2, (-50 + (100 * (10 - i))) / 2); polygon.addPoint((195 + (145 * u + count))
-	 * / 2, (-50 + (100 * (10 - i))) / 2);
-	 * 
-	 * polygon.addPoint((95 + (145 * u + count)) / 2, (50 + (100 * (10 - i))) / 2);
-	 * polygon.addPoint((-50 + (145 * u + count)) / 2, (50 + (100 * (10 - i))) / 2);
-	 * 
-	 * g2.setColor(Color.GREEN); g2.fillPolygon(polygon);
-	 * 
-	 * } else if (map[i][u].getCode().equals("river")) {
-	 * 
-	 * Polygon polygon = new Polygon(); polygon.addPoint((50 + (145 * u + count)) /
-	 * 2, (-50 + (100 * (10 - i))) / 2); polygon.addPoint((195 + (145 * u + count))
-	 * / 2, (-50 + (100 * (10 - i))) / 2);
-	 * 
-	 * polygon.addPoint((95 + (145 * u + count)) / 2, (50 + (100 * (10 - i))) / 2);
-	 * polygon.addPoint((-50 + (145 * u + count)) / 2, (50 + (100 * (10 - i))) / 2);
-	 * 
-	 * g2.setColor(Color.BLUE); g2.fillPolygon(polygon);
-	 * 
-	 * } else if (map[i][u].getCode().equals("bridg")) {
-	 * 
-	 * Polygon polygon = new Polygon(); polygon.addPoint((50 + (145 * u + count)) /
-	 * 2, (-50 + (100 * (10 - i))) / 2); polygon.addPoint((195 + (145 * u + count))
-	 * / 2, (-50 + (100 * (10 - i))) / 2);
-	 * 
-	 * polygon.addPoint((95 + (145 * u + count)) / 2, (50 + (100 * (10 - i))) / 2);
-	 * polygon.addPoint((-50 + (145 * u + count)) / 2, (50 + (100 * (10 - i))) / 2);
-	 * 
-	 * g2.setColor(Color.YELLOW); g2.fillPolygon(polygon);
-	 * 
-	 * } else if (map[i][u].getCode().equals("walls")) {
-	 * 
-	 * Polygon polygon = new Polygon(); polygon.addPoint((50 + (145 * u + count)) /
-	 * 2, (-50 + (100 * (10 - i))) / 2); polygon.addPoint((195 + (145 * u + count))
-	 * / 2, (-50 + (100 * (10 - i))) / 2);
-	 * 
-	 * polygon.addPoint((95 + (145 * u + count)) / 2, (50 + (100 * (10 - i))) / 2);
-	 * polygon.addPoint((-50 + (145 * u + count)) / 2, (50 + (100 * (10 - i))) / 2);
-	 * 
-	 * g2.setColor(Color.GRAY); g2.fillPolygon(polygon);
-	 * 
-	 * } else if (map[i][u].getCode().equals("fight")) {
-	 * 
-	 * Polygon polygon = new Polygon(); polygon.addPoint((50 + (145 * u + count)) /
-	 * 2, (-50 + (100 * (10 - i))) / 2); polygon.addPoint((195 + (145 * u + count))
-	 * / 2, (-50 + (100 * (10 - i))) / 2);
-	 * 
-	 * polygon.addPoint((95 + (145 * u + count)) / 2, (50 + (100 * (10 - i))) / 2);
-	 * polygon.addPoint((-50 + (145 * u + count)) / 2, (50 + (100 * (10 - i))) / 2);
-	 * 
-	 * g2.setColor(Color.MAGENTA); g2.fillPolygon(polygon);
-	 * 
-	 * } else {
-	 * 
-	 * Polygon polygon = new Polygon(); polygon.addPoint((50 + (145 * u + count)) /
-	 * 2, (-50 + (100 * (10 - i))) / 2); polygon.addPoint((195 + (145 * u + count))
-	 * / 2, (-50 + (100 * (10 - i))) / 2);
-	 * 
-	 * polygon.addPoint((95 + (145 * u + count)) / 2, (50 + (100 * (10 - i))) / 2);
-	 * polygon.addPoint((-50 + (145 * u + count)) / 2, (50 + (100 * (10 - i))) / 2);
-	 * 
-	 * g2.setColor(Color.BLACK); g2.fillPolygon(polygon);
-	 * 
-	 * }
-	 * 
-	 * g2.setColor(Color.BLACK);
-	 * 
-	 * // Recta superior g2.drawLine((50 + (145 * u + count)) / 2, (-50 + (100 * (10
-	 * - i))) / 2, (195 + (145 * u + count)) / 2, (-50 + (100 * (10 - i))) / 2);
-	 * 
-	 * // Recta inferior g2.drawLine((-50 + (145 * u + count)) / 2, (50 + (100 * (10
-	 * - i))) / 2, (95 + (145 * u + count)) / 2, (50 + (100 * (10 - i))) / 2);
-	 * 
-	 * // Diagonal izquierda g2.drawLine((50 + (145 * u + count)) / 2, (-50 + (100 *
-	 * (10 - i))) / 2, (-50 + (145 * u + count)) / 2, (50 + (100 * (10 - i))) / 2);
-	 * 
-	 * // Diagonal derecha g2.drawLine((195 + (145 * u + count)) / 2, (-50 + (100 *
-	 * (10 - i))) / 2, (95 + (145 * u + count)) / 2, (50 + (100 * (10 - i))) / 2);
-	 * 
-	 * } count = count + 100; }
-	 * 
-	 * }
-	 **/
 
 	public void dibujado() {
 
 		repaint();
 
-		if (map[x][y].getCode() == ("fight")) {
+		if (mundo[x][y].getCode() == ("fight")) {
 
 			JOptionPane.showMessageDialog(null, "Pelea");
-			
+
 			x = x_ant;
 			y = y_ant;
 
@@ -253,16 +138,14 @@ public class Array extends JFrame implements KeyListener {
 				JLabel mapa = new JLabel();
 				JLabel perso = new JLabel();
 				JLabel pelea = new JLabel();
-				
 
-				if (map[i][u].getCode().equals("grass")) {
+				if (mundo[i + x_dib][u + y_dib].getCode().equals("grass")) {
 
 					mapa.setIcon(new ImageIcon(prueba.class.getResource("/Imagenes/hierba.png")));
 
-				} else if (map[i][u].getCode().equals("walls")) {
+				} else if (mundo[i + x_dib][u + y_dib].getCode().equals("walls")) {
 
-					if (u == 0 || u == 9) {
-
+					if ((u == 0 || u == 9) && (i != 0 && i != 9)) {
 						mapa.setIcon(new ImageIcon(prueba.class.getResource("/Imagenes/paredgirada.png")));
 
 					} else {
@@ -270,17 +153,19 @@ public class Array extends JFrame implements KeyListener {
 						mapa.setIcon(new ImageIcon(prueba.class.getResource("/Imagenes/pared.png")));
 					}
 
-				} else if (map[i][u].getCode().equals("river")) {
+				} else if (mundo[i + x_dib][u + y_dib].getCode().equals("river")) {
 
-					//mapa.setIcon(new ImageIcon("C:\\Users\\david\\OneDrive\\Escritorio\\UNI\\Proceso de software\\water.gif"));
-					
+					// mapa.setIcon(new
+					// ImageIcon("C:\\Users\\david\\OneDrive\\Escritorio\\UNI\\Proceso de
+					// software\\water.gif"));
+
 					mapa.setIcon(new ImageIcon(prueba.class.getResource("/Imagenes/Agua.png")));
 
-				} else if (map[i][u].getCode().equals("bridg")) {
+				} else if (mundo[i + x_dib][u + y_dib].getCode().equals("bridg")) {
 
 					mapa.setIcon(new ImageIcon(prueba.class.getResource("/Imagenes/bridge.png")));
 
-				} else if (map[i][u].getCode().equals("fight")) {
+				} else if (mundo[i + x_dib][u + y_dib].getCode().equals("fight")) {
 
 					mapa.setIcon(new ImageIcon(prueba.class.getResource("/Imagenes/hierba.png")));
 					pelea.setIcon(new ImageIcon(prueba.class.getResource("/Imagenes/calabera.png")));
@@ -290,9 +175,33 @@ public class Array extends JFrame implements KeyListener {
 					mapa.setBackground(Color.BLACK);
 				}
 
-				if (i == x && u == y) {
+				if (i == x - x_dib && u == y - y_dib) {
 
-					perso.setIcon(new ImageIcon(prueba.class.getResource("/Imagenes/transparente.png")));
+					// Personajes
+
+					// Caballero
+					// perso.setIcon(new
+					// ImageIcon(prueba.class.getResource("/Imagenes/caballero.png")));
+
+					// Arquero
+					if (direcccion.equals("UP")) {
+
+						perso.setIcon(new ImageIcon(prueba.class.getResource("/Imagenes/arquero_detras_sf.png")));
+
+					} else if (direcccion.equals("DOWN")) {
+
+						perso.setIcon(new ImageIcon(prueba.class.getResource("/Imagenes/arquero_delante_sf.png")));
+
+					} else if (direcccion.equals("RIGHT")) {
+
+						perso.setIcon(new ImageIcon(prueba.class.getResource("/Imagenes/arquero.png")));
+
+					} else if (direcccion.equals("LEFT")) {
+
+						perso.setIcon(new ImageIcon(prueba.class.getResource("/Imagenes/arquero_girado_sf.png")));
+
+					}
+
 				}
 
 				pos_x = pos_x + 60;
@@ -306,35 +215,24 @@ public class Array extends JFrame implements KeyListener {
 			}
 		}
 
-		// Consola
-		System.out.println();
-		System.out.println();
-		System.out.println();
-		System.out.println();
-
-		for (int i = 0; i < 10; i++) {
-			System.out.println();
-			for (int u = 0; u < 10; u++) {
-
-				if (i == x && u == y) {
-
-					System.out.print("perso");
-					System.out.print("  ");
-
-				} else {
-					System.out.print(map[i][u].getCode());
-					System.out.print("  ");
-				}
-
-			}
-		}
-
-		System.out.println();
-		System.out.println();
-		System.out.println();
-		System.out.println();
-		System.out.println();
-		System.out.println();
+		/**
+		 * // Consola System.out.println(); System.out.println(); System.out.println();
+		 * System.out.println();
+		 * 
+		 * for (int i = 0; i < 50; i++) { System.out.println(); for (int u = 0; u < 50;
+		 * u++) {
+		 * 
+		 * if (i == x && u == y) {
+		 * 
+		 * System.out.print("perso"); System.out.print(" ");
+		 * 
+		 * } else { System.out.print(mundo[i][u].getCode()); System.out.print(" "); }
+		 * 
+		 * } }
+		 * 
+		 * System.out.println(); System.out.println(); System.out.println();
+		 * System.out.println(); System.out.println(); System.out.println();
+		 **/
 	}
 
 	public void keyPressed(KeyEvent e) {
@@ -343,57 +241,73 @@ public class Array extends JFrame implements KeyListener {
 
 		if (key == KeyEvent.VK_DOWN) {
 
-			if (x < 8) {
+			if (x <= 47) {
 
-				if (map[x + 1][y].isDown()) {
+				if (mundo[x + 1][y].isDown()) {
 
 					x_ant = x;
 					y_ant = y;
 					x = x + 1;
-					dibujado();
+					if (x > 5 && x < 46) {
+						x_dib = x_dib + 1;
+					}
 				}
-
+				direcccion = "DOWN";
+				dibujado();
 			}
 
 		} else if (key == KeyEvent.VK_LEFT) {
 
-			if (y > 1) {
+			if (y >= 2) {
 
-				if (map[x][y - 1].isLeft()) {
+				if (mundo[x][y - 1].isLeft()) {
 
 					x_ant = x;
 					y_ant = y;
 					y = y - 1;
-					dibujado();
+					if (y > 4 && y < 45) {
+						y_dib = y_dib - 1;
+					}
 				}
-
+				direcccion = "LEFT";
+				dibujado();
 			}
 
 		} else if (key == KeyEvent.VK_RIGHT) {
 
-			if (y < 8) {
+			if (y <= 47) {
 
-				if (map[x][y + 1].isRight()) {
+				if (mundo[x][y + 1].isRight()) {
 
 					x_ant = x;
 					y_ant = y;
 					y = y + 1;
-					dibujado();
+					if (y > 5 && y < 46) {
+						y_dib = y_dib + 1;
+					}
+
 				}
+				direcccion = "RIGHT";
+				dibujado();
 
 			}
 
 		} else if (key == KeyEvent.VK_UP) {
 
-			if (x > 1) {
+			if (x >= 2) {
 
-				if (map[x - 1][y].isUp()) {
+				if (mundo[x - 1][y].isUp()) {
 
 					x_ant = x;
 					y_ant = y;
 					x = x - 1;
-					dibujado();
+					if (x < 45 && x > 4) {
+						x_dib = x_dib - 1;
+					}
 				}
+
+				direcccion = "UP";
+				dibujado();
 
 			}
 
