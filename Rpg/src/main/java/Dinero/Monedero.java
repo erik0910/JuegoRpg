a7a7bebdffb6d7e0ca8c135b964dfcf75e6df0d7
erik0910 <erikmartinez0910@gmail.com>
2021-04-06 20:00:32 +0200

    clase mondero creada para poder funcionar lista

---
+++ Rpg/src/main/java/Dinero/Monedero.java
@@ -0,0 +1,34 @@
+package Dinero;
+
+public class Monedero {
+/*
+ * Clase moneda encragada de  hacer toda la gestión de 
+ * */
+ private int dinero;
+
+public int getDinero() {
+	return dinero;
+}
+
+public void setDinero(int dinero) {
+	this.dinero = dinero;
+}
+
+public Monedero() {
+	super();
+	this.dinero = 1000;// ponemos por default la monedas que va a tener 
+}
+//metodo que se activara cuando el personaje ha ganado
+public void ganar() {
+	this.dinero += 100;
+}
+//metodo que se activa si  el personaje a perdido
+public void perder() {
+	this.dinero -=100;
+}
+@Override
+public String toString() {
+	return "Monedero [dinero=" + dinero + "]";
+}
+ 
+}
