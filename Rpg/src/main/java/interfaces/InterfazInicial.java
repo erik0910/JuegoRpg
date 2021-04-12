6dd802f3d04e3aaf874c1216bb633531bcfb797b
Iker <iker.tobal@opendeusto.es>
2021-04-12 20:58:43 +0200

    ajuste gitignore

--- Rpg/src/main/java/interfaces/InterfazInicial.java
+++ Rpg/src/main/java/interfaces/InterfazInicial.java
@@ -2,21 +2,16 @@ package interfaces;
 
 import java.awt.Color;
 import java.awt.EventQueue;
-import javax.swing.JFrame;
-import javax.swing.border.EmptyBorder;
-import java.awt.GridBagLayout;
-
-import javax.swing.ImageIcon;
-import javax.swing.JButton;
-import java.awt.GridBagConstraints;
-import java.awt.Insets;
-import java.awt.SystemColor;
-import java.awt.event.ActionListener;
-import java.awt.event.ActionEvent;
 import java.awt.Font;
-import javax.swing.SwingConstants;
+import java.awt.SystemColor;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import javax.swing.JButton;
+import javax.swing.JFrame;
 import javax.swing.JLabel;
-import javax.swing.JPanel;
+import javax.swing.SwingConstants;
+import javax.swing.border.EmptyBorder;
 
 public class InterfazInicial extends JFrame {
 
