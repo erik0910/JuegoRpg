bd2fa38e9ec70e23f26ba37a78b114257ea177e9
xabie <xabie@192.168.1.39>
2021-04-26 11:27:50 +0200

    Ventanas Stats de Armas

--- Rpg/src/main/java/interfaces/InterfazTienda.java
+++ Rpg/src/main/java/interfaces/InterfazTienda.java
@@ -8,6 +8,7 @@ import java.awt.event.ActionListener;
 
 import javax.swing.ImageIcon;
 import javax.swing.JButton;
+import javax.swing.JTextPane;
 
 public class InterfazTienda extends JFrame{
 	public InterfazTienda() {
@@ -78,40 +79,70 @@ public class InterfazTienda extends JFrame{
 		btnMalla2.setBounds(826, 780, 137, 33);
 		getContentPane().add(btnMalla2);
 		
-		JButton btnPropEspada = new JButton("PROPIEDADES");
+		JButton btnPropEspada = new JButton("STATS");
 		btnPropEspada.setFont(new Font("Times New Roman", Font.PLAIN, 12));
-		btnPropEspada.setBounds(163, 404, 115, 33);
+		btnPropEspada.setBounds(163, 404, 96, 33);
 		getContentPane().add(btnPropEspada);
 		
-		JButton btnPropEspada2 = new JButton("PROPIEDADES");
+		JButton btnPropEspada2 = new JButton("STATS");
 		btnPropEspada2.setFont(new Font("Times New Roman", Font.PLAIN, 12));
-		btnPropEspada2.setBounds(163, 780, 115, 33);
+		btnPropEspada2.setBounds(163, 780, 96, 33);
 		getContentPane().add(btnPropEspada2);
 		
-		JButton btnPropLanza = new JButton("PROPIEDADES");
+		JButton btnPropLanza = new JButton("STATS");
 		btnPropLanza.setFont(new Font("Times New Roman", Font.PLAIN, 12));
-		btnPropLanza.setBounds(560, 404, 115, 33);
+		btnPropLanza.setBounds(560, 404, 93, 33);
 		getContentPane().add(btnPropLanza);
 		
-		JButton btnPropLanza2 = new JButton("PROPIEDADES");
+		JButton btnPropLanza2 = new JButton("STATS");
 		btnPropLanza2.setFont(new Font("Times New Roman", Font.PLAIN, 12));
-		btnPropLanza2.setBounds(560, 780, 115, 33);
+		btnPropLanza2.setBounds(560, 780, 93, 33);
 		getContentPane().add(btnPropLanza2);
 		
-		JButton btnPropMalla = new JButton("PROPIEDADES");
+		JButton btnPropMalla = new JButton("STATS");
 		btnPropMalla.setFont(new Font("Times New Roman", Font.PLAIN, 12));
-		btnPropMalla.setBounds(973, 404, 115, 33);
+		btnPropMalla.setBounds(973, 404, 96, 33);
 		getContentPane().add(btnPropMalla);
 		
-		JButton btnPropMalla2 = new JButton("PROPIEDADES");
+		JButton btnPropMalla2 = new JButton("STATS");
 		btnPropMalla2.setFont(new Font("Times New Roman", Font.PLAIN, 12));
-		btnPropMalla2.setBounds(974, 780, 115, 33);
+		btnPropMalla2.setBounds(974, 780, 96, 33);
 		getContentPane().add(btnPropMalla2);	
 		
 		JButton btnSalida = new JButton("SALIR");
 		btnSalida.setFont(new Font("Times New Roman", Font.PLAIN, 12));
-		btnSalida.setBounds(10, 10, 125, 52);
+		btnSalida.setBounds(10, 10, 96, 33);
 		getContentPane().add(btnSalida);
+		
+		JLabel lblNewLabel = new JLabel("1200$");
+		lblNewLabel.setFont(new Font("Times New Roman", Font.PLAIN, 25));
+		lblNewLabel.setBounds(119, 72, 71, 29);
+		getContentPane().add(lblNewLabel);
+		
+		JLabel lblNewLabel_1 = new JLabel("1500$");
+		lblNewLabel_1.setFont(new Font("Times New Roman", Font.PLAIN, 25));
+		lblNewLabel_1.setBounds(511, 69, 71, 29);
+		getContentPane().add(lblNewLabel_1);
+		
+		JLabel lblNewLabel_2 = new JLabel("1400$");
+		lblNewLabel_2.setFont(new Font("Times New Roman", Font.PLAIN, 25));
+		lblNewLabel_2.setBounds(924, 72, 71, 29);
+		getContentPane().add(lblNewLabel_2);
+		
+		JLabel lblNewLabel_3 = new JLabel("2500$");
+		lblNewLabel_3.setFont(new Font("Times New Roman", Font.PLAIN, 25));
+		lblNewLabel_3.setBounds(106, 466, 71, 29);
+		getContentPane().add(lblNewLabel_3);
+		
+		JLabel lblNewLabel_3_1 = new JLabel("2000$");
+		lblNewLabel_3_1.setFont(new Font("Times New Roman", Font.PLAIN, 25));
+		lblNewLabel_3_1.setBounds(511, 466, 71, 29);
+		getContentPane().add(lblNewLabel_3_1);
+		
+		JLabel lblNewLabel_3_1_1 = new JLabel("2100$");
+		lblNewLabel_3_1_1.setFont(new Font("Times New Roman", Font.PLAIN, 25));
+		lblNewLabel_3_1_1.setBounds(924, 466, 71, 29);
+		getContentPane().add(lblNewLabel_3_1_1);
 		
 		btnSalida.addActionListener(new ActionListener() {
 			
@@ -127,7 +158,7 @@ public class InterfazTienda extends JFrame{
 			public void actionPerformed(ActionEvent e) {
 				InterfazTienda v1 = new InterfazTienda();
 				v1.setVisible(true);
-				
+			
 			}
 		});
 		
@@ -187,6 +218,10 @@ public class InterfazTienda extends JFrame{
 				// TODO Auto-generated method stub
 				InterfazTienda i1 = new InterfazTienda();
 				i1.setVisible(true);
+				JLabel lblStats = new JLabel("Daño: +5; Speed: +1");
+				lblStats.setFont(new Font("Times New Roman", Font.PLAIN, 25));
+				lblStats.setBounds(511, 466, 71, 29);
+				getContentPane().add(lblStats);
 			}
 		});
 		btnPropEspada2.addActionListener(new ActionListener() {
@@ -195,6 +230,10 @@ public class InterfazTienda extends JFrame{
 				// TODO Auto-generated method stub
 				InterfazTienda i2 = new InterfazTienda();
 				i2.setVisible(true);
+				JLabel lblStats = new JLabel("Daño: +12; Speed: +2");
+				lblStats.setFont(new Font("Times New Roman", Font.PLAIN, 25));
+				lblStats.setBounds(511, 466, 71, 29);
+				getContentPane().add(lblStats);
 			}
 		});
 		btnPropLanza.addActionListener(new ActionListener() {
@@ -203,6 +242,10 @@ public class InterfazTienda extends JFrame{
 				// TODO Auto-generated method stub
 				InterfazTienda i3 = new InterfazTienda();
 				i3.setVisible(true);
+				JLabel lblStats = new JLabel("Daño: +2; Speed: +3");
+				lblStats.setFont(new Font("Times New Roman", Font.PLAIN, 25));
+				lblStats.setBounds(511, 466, 71, 29);
+				getContentPane().add(lblStats);
 			}
 		});
 		btnPropLanza2.addActionListener(new ActionListener() {
@@ -211,6 +254,10 @@ public class InterfazTienda extends JFrame{
 				// TODO Auto-generated method stub
 				InterfazTienda i4 = new InterfazTienda();
 				i4.setVisible(true);
+				JLabel lblStats = new JLabel("Daño: +4; Speed: +5");
+				lblStats.setFont(new Font("Times New Roman", Font.PLAIN, 25));
+				lblStats.setBounds(511, 466, 71, 29);
+				getContentPane().add(lblStats);
 			}
 		});
 		btnPropMalla.addActionListener(new ActionListener() {
@@ -219,6 +266,10 @@ public class InterfazTienda extends JFrame{
 				// TODO Auto-generated method stub
 				InterfazTienda i5 = new InterfazTienda();
 				i5.setVisible(true);
+				JLabel lblStats = new JLabel("Speed: +7");
+				lblStats.setFont(new Font("Times New Roman", Font.PLAIN, 25));
+				lblStats.setBounds(511, 466, 71, 29);
+				getContentPane().add(lblStats);
 			}
 		});
 		btnPropMalla2.addActionListener(new ActionListener() {
@@ -227,6 +278,10 @@ public class InterfazTienda extends JFrame{
 				// TODO Auto-generated method stub
 				InterfazTienda i6 = new InterfazTienda();
 				i6.setVisible(true);
+				JLabel lblStats = new JLabel("Speed: +12");
+				lblStats.setFont(new Font("Times New Roman", Font.PLAIN, 25));
+				lblStats.setBounds(511, 466, 71, 29);
+				getContentPane().add(lblStats);
 			}
 		});
 		}
