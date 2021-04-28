702df1acb031678b0ad3d1e4a4e1334aca243e1b
xabier25 <43268968+xabier25@users.noreply.github.com>
2021-04-28 19:29:51 +0200

    Update InterfazTienda.java

--- Rpg/src/main/java/interfaces/InterfazTienda.java
+++ Rpg/src/main/java/interfaces/InterfazTienda.java
@@ -2,287 +2,310 @@ package interfaces;
 
 import javax.swing.JFrame;
 import javax.swing.JLabel;
+import javax.swing.JOptionPane;
+import javax.swing.JPanel;
+import javax.swing.ImageIcon;
+
+import java.awt.Color;
+import java.awt.EventQueue;
 import java.awt.Font;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
-import javax.swing.ImageIcon;
 import javax.swing.JButton;
 import javax.swing.JTextPane;
+import javax.swing.border.EmptyBorder;
 
-public class InterfazTienda extends JFrame{
+import mapa.Array;
+
+public class InterfazTienda extends JFrame {
+
+	static final ClassLoader loader = InterfazTienda.class.getClassLoader();
+	public static FondoTiendaCargar contentPane;
+
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					InterfazTienda frame = new InterfazTienda();
+					frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
 	public InterfazTienda() {
+		contentPane = new FondoTiendaCargar();
+		contentPane.setBackground(Color.WHITE);
+		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
+		setContentPane(contentPane);
+		contentPane.setLayout(null);
+		
+		
+		setBounds(100, 100, 700, 700);
+		setResizable(false);
 		getContentPane().setLayout(null);
-		
+
 		JLabel lblTitulo = new JLabel("TIENDA");
-		lblTitulo.setFont(new Font("Times New Roman", Font.BOLD | Font.ITALIC, 50));
-		lblTitulo.setBounds(449, 10, 191, 52);
+		lblTitulo.setFont(new Font("Algerian", Font.BOLD | Font.ITALIC, 40));
+		lblTitulo.setBounds(270, 0, 156, 39);
 		getContentPane().add(lblTitulo);
-		
-		JLabel lblEspada = new JLabel("");
-		lblEspada.setIcon(new ImageIcon(InterfazTienda.class.getResource("/resources/Espada.png")));
-		lblEspada.setBounds(10, 108, 250, 245);
-		getContentPane().add(lblEspada);
-		
-		JLabel lblLanza = new JLabel("");
-		lblLanza.setIcon(new ImageIcon(InterfazTienda.class.getResource("/resources/lanza.png")));
-		lblLanza.setBounds(425, 108, 250, 250);
-		getContentPane().add(lblLanza);
-		
-		JLabel lblMalla = new JLabel("");
-		lblMalla.setIcon(new ImageIcon(InterfazTienda.class.getResource("/resources/malla.png")));
-		lblMalla.setBounds(826, 105, 250, 250);
-		getContentPane().add(lblMalla);
-		
-		JLabel lblEspada2 = new JLabel("");
-		lblEspada2.setIcon(new ImageIcon(InterfazTienda.class.getResource("/resources/espada2.png")));
-		lblEspada2.setBounds(10, 505, 250, 245);
-		getContentPane().add(lblEspada2);
-		
-		JLabel lblLanza2 = new JLabel("");
-		lblLanza2.setIcon(new ImageIcon(InterfazTienda.class.getResource("/resources/lanza 2.png")));
-		lblLanza2.setBounds(397, 494, 256, 256);
-		getContentPane().add(lblLanza2);
-		
-		JLabel lblMalla2 = new JLabel("");
-		lblMalla2.setIcon(new ImageIcon(InterfazTienda.class.getResource("/resources/malla2.png")));
-		lblMalla2.setBounds(820, 510, 256, 240);
-		getContentPane().add(lblMalla2);
-		
-		JButton btnEspada = new JButton("COMPRAR ESPADA");
-		btnEspada.setFont(new Font("Times New Roman", Font.PLAIN, 12));
-		btnEspada.setBounds(10, 404, 143, 33);
+
+		JButton btnEspada = new JButton("COMPRAR");
+		btnEspada.setBackground(new Color(205, 133, 63));
+		btnEspada.setForeground(new Color(0, 0, 0));
+		btnEspada.setFont(new Font("Algerian", Font.PLAIN, 10));
+		btnEspada.setBounds(25, 329, 93, 33);
 		getContentPane().add(btnEspada);
-		
-		JButton btnEspada2 = new JButton("COMPRAR ESPADA");
-		btnEspada2.setFont(new Font("Times New Roman", Font.PLAIN, 12));
-		btnEspada2.setBounds(10, 780, 143, 33);
+
+		JButton btnEspada2 = new JButton("COMPRAR");
+		btnEspada2.setBackground(new Color(205, 133, 63));
+		btnEspada2.setFont(new Font("Algerian", Font.PLAIN, 10));
+		btnEspada2.setBounds(25, 629, 93, 33);
 		getContentPane().add(btnEspada2);
-		
-		JButton btnLanza = new JButton("COMPRAR LANZA");
-		btnLanza.setFont(new Font("Times New Roman", Font.PLAIN, 12));
-		btnLanza.setBounds(409, 404, 137, 33);
+
+		JButton btnLanza = new JButton("COMPRAR");
+		btnLanza.setBackground(new Color(205, 133, 63));
+		btnLanza.setFont(new Font("Algerian", Font.PLAIN, 10));
+		btnLanza.setBounds(263, 329, 92, 33);
 		getContentPane().add(btnLanza);
-		
-		JButton btnLanza2 = new JButton("COMPRAR LANZA");
-		btnLanza2.setFont(new Font("Times New Roman", Font.PLAIN, 12));
-		btnLanza2.setBounds(409, 780, 137, 33);
+
+		JButton btnLanza2 = new JButton("COMPRAR");
+		btnLanza2.setBackground(new Color(205, 133, 63));
+		btnLanza2.setFont(new Font("Algerian", Font.PLAIN, 10));
+		btnLanza2.setBounds(263, 629, 92, 33);
 		getContentPane().add(btnLanza2);
-		
-		JButton btnMalla = new JButton("COMPRAR MALLA");
-		btnMalla.setFont(new Font("Times New Roman", Font.PLAIN, 12));
-		btnMalla.setBounds(826, 404, 137, 33);
+
+		JButton btnMalla = new JButton("COMPRAR\r\n");
+		btnMalla.setBackground(new Color(205, 133, 63));
+		btnMalla.setFont(new Font("Algerian", Font.PLAIN, 10));
+		btnMalla.setBounds(492, 329, 93, 33);
 		getContentPane().add(btnMalla);
-		
-		JButton btnMalla2 = new JButton("COMPRAR MALLA");
-		btnMalla2.setFont(new Font("Times New Roman", Font.PLAIN, 12));
-		btnMalla2.setBounds(826, 780, 137, 33);
+
+		JButton btnMalla2 = new JButton("COMPRAR");
+		btnMalla2.setBackground(new Color(205, 133, 63));
+		btnMalla2.setFont(new Font("Algerian", Font.PLAIN, 10));
+		btnMalla2.setBounds(492, 629, 93, 33);
 		getContentPane().add(btnMalla2);
-		
+
 		JButton btnPropEspada = new JButton("STATS");
-		btnPropEspada.setFont(new Font("Times New Roman", Font.PLAIN, 12));
-		btnPropEspada.setBounds(163, 404, 96, 33);
+		btnPropEspada.setBackground(new Color(205, 133, 63));
+		btnPropEspada.setFont(new Font("Algerian", Font.PLAIN, 10));
+		btnPropEspada.setBounds(128, 329, 77, 33);
 		getContentPane().add(btnPropEspada);
-		
+
 		JButton btnPropEspada2 = new JButton("STATS");
-		btnPropEspada2.setFont(new Font("Times New Roman", Font.PLAIN, 12));
-		btnPropEspada2.setBounds(163, 780, 96, 33);
+		btnPropEspada2.setBackground(new Color(205, 133, 63));
+		btnPropEspada2.setFont(new Font("Algerian", Font.PLAIN, 10));
+		btnPropEspada2.setBounds(128, 629, 77, 33);
 		getContentPane().add(btnPropEspada2);
-		
+
 		JButton btnPropLanza = new JButton("STATS");
-		btnPropLanza.setFont(new Font("Times New Roman", Font.PLAIN, 12));
-		btnPropLanza.setBounds(560, 404, 93, 33);
+		btnPropLanza.setBackground(new Color(205, 133, 63));
+		btnPropLanza.setFont(new Font("Algerian", Font.PLAIN, 10));
+		btnPropLanza.setBounds(365, 329, 77, 33);
 		getContentPane().add(btnPropLanza);
-		
+
 		JButton btnPropLanza2 = new JButton("STATS");
-		btnPropLanza2.setFont(new Font("Times New Roman", Font.PLAIN, 12));
-		btnPropLanza2.setBounds(560, 780, 93, 33);
+		btnPropLanza2.setBackground(new Color(205, 133, 63));
+		btnPropLanza2.setFont(new Font("Algerian", Font.PLAIN, 10));
+		btnPropLanza2.setBounds(365, 629, 77, 33);
 		getContentPane().add(btnPropLanza2);
-		
+
 		JButton btnPropMalla = new JButton("STATS");
-		btnPropMalla.setFont(new Font("Times New Roman", Font.PLAIN, 12));
-		btnPropMalla.setBounds(973, 404, 96, 33);
+		btnPropMalla.setBackground(new Color(205, 133, 63));
+		btnPropMalla.setFont(new Font("Algerian", Font.PLAIN, 10));
+		btnPropMalla.setBounds(595, 328, 77, 34);
 		getContentPane().add(btnPropMalla);
-		
+
 		JButton btnPropMalla2 = new JButton("STATS");
-		btnPropMalla2.setFont(new Font("Times New Roman", Font.PLAIN, 12));
-		btnPropMalla2.setBounds(974, 780, 96, 33);
-		getContentPane().add(btnPropMalla2);	
-		
+		btnPropMalla2.setBackground(new Color(205, 133, 63));
+		btnPropMalla2.setFont(new Font("Algerian", Font.PLAIN, 10));
+		btnPropMalla2.setBounds(595, 629, 77, 33);
+		getContentPane().add(btnPropMalla2);
+
 		JButton btnSalida = new JButton("SALIR");
-		btnSalida.setFont(new Font("Times New Roman", Font.PLAIN, 12));
-		btnSalida.setBounds(10, 10, 96, 33);
+		btnSalida.setBackground(new Color(210, 105, 30));
+		btnSalida.setFont(new Font("Algerian", Font.PLAIN, 12));
+		btnSalida.setBounds(10, 12, 83, 33);
 		getContentPane().add(btnSalida);
-		
-		JLabel lblNewLabel = new JLabel("1200$");
-		lblNewLabel.setFont(new Font("Times New Roman", Font.PLAIN, 25));
-		lblNewLabel.setBounds(119, 72, 71, 29);
+
+		JLabel lblNewLabel = new JLabel("Espada nivel 1: 1200$");
+		lblNewLabel.setFont(new Font("Algerian", Font.PLAIN, 15));
+		lblNewLabel.setBounds(46, 73, 159, 29);
 		getContentPane().add(lblNewLabel);
-		
-		JLabel lblNewLabel_1 = new JLabel("1500$");
-		lblNewLabel_1.setFont(new Font("Times New Roman", Font.PLAIN, 25));
-		lblNewLabel_1.setBounds(511, 69, 71, 29);
+
+		JLabel lblNewLabel_1 = new JLabel("Lanza nivel 1: 1500$");
+		lblNewLabel_1.setFont(new Font("Algerian", Font.PLAIN, 15));
+		lblNewLabel_1.setBounds(283, 73, 143, 29);
 		getContentPane().add(lblNewLabel_1);
-		
-		JLabel lblNewLabel_2 = new JLabel("1400$");
-		lblNewLabel_2.setFont(new Font("Times New Roman", Font.PLAIN, 25));
-		lblNewLabel_2.setBounds(924, 72, 71, 29);
+
+		JLabel lblNewLabel_2 = new JLabel("Malla nivel 1: 1400$");
+		lblNewLabel_2.setFont(new Font("Algerian", Font.PLAIN, 15));
+		lblNewLabel_2.setBounds(518, 73, 145, 29);
 		getContentPane().add(lblNewLabel_2);
-		
-		JLabel lblNewLabel_3 = new JLabel("2500$");
-		lblNewLabel_3.setFont(new Font("Times New Roman", Font.PLAIN, 25));
-		lblNewLabel_3.setBounds(106, 466, 71, 29);
+
+		JLabel lblNewLabel_3 = new JLabel("Espada nivel 2: 2500$");
+		lblNewLabel_3.setFont(new Font("Algerian", Font.PLAIN, 15));
+		lblNewLabel_3.setBounds(46, 372, 159, 29);
 		getContentPane().add(lblNewLabel_3);
-		
-		JLabel lblNewLabel_3_1 = new JLabel("2000$");
-		lblNewLabel_3_1.setFont(new Font("Times New Roman", Font.PLAIN, 25));
-		lblNewLabel_3_1.setBounds(511, 466, 71, 29);
+
+		JLabel lblNewLabel_3_1 = new JLabel("Lanza nivel 2: 2000$");
+		lblNewLabel_3_1.setFont(new Font("Algerian", Font.PLAIN, 15));
+		lblNewLabel_3_1.setBounds(283, 372, 143, 29);
 		getContentPane().add(lblNewLabel_3_1);
-		
-		JLabel lblNewLabel_3_1_1 = new JLabel("2100$");
-		lblNewLabel_3_1_1.setFont(new Font("Times New Roman", Font.PLAIN, 25));
-		lblNewLabel_3_1_1.setBounds(924, 466, 71, 29);
+
+		JLabel lblNewLabel_3_1_1 = new JLabel("Malla nivel 2: 2100$");
+		lblNewLabel_3_1_1.setFont(new Font("Algerian", Font.PLAIN, 15));
+		lblNewLabel_3_1_1.setBounds(518, 372, 145, 29);
 		getContentPane().add(lblNewLabel_3_1_1);
-		
+
+		JLabel lblEspada = new JLabel("");
+		lblEspada.setBounds(46, 128, 150, 150);
+		contentPane.add(lblEspada);
+		ImageIcon imagenIcono = new ImageIcon(loader.getResource("tienda/espada.png"));
+		lblEspada.setIcon(imagenIcono);
+
+		JLabel lblLanza = new JLabel("");
+		lblLanza.setBounds(283, 142, 150, 150);
+		contentPane.add(lblLanza);
+		ImageIcon imagenLanza = new ImageIcon(loader.getResource("tienda/lanza.png"));
+		lblLanza.setIcon(imagenLanza);
+
+		JLabel lblMalla = new JLabel("");
+		lblMalla.setBounds(513, 142, 150, 150);
+		contentPane.add(lblMalla);
+		ImageIcon imagenMalla = new ImageIcon(loader.getResource("tienda/malla.png"));
+		lblMalla.setIcon(imagenMalla);
+
+		JLabel lblEspada2 = new JLabel("");
+		lblEspada2.setBounds(46, 434, 150, 150);
+		contentPane.add(lblEspada2);
+		ImageIcon imagenEspada2 = new ImageIcon(loader.getResource("tienda/espada2.png"));
+		lblEspada2.setIcon(imagenEspada2);
+
+		JLabel lblLanza2 = new JLabel("");
+		lblLanza2.setBounds(270, 434, 150, 150);
+		contentPane.add(lblLanza2);
+		ImageIcon imagenLanza2 = new ImageIcon(loader.getResource("tienda/lanza2.png"));
+		lblLanza2.setIcon(imagenLanza2);
+
+		JLabel lblMalla2 = new JLabel("");
+		lblMalla2.setBounds(518, 434, 150, 150);
+		contentPane.add(lblMalla2);
+		ImageIcon imagenMalla2 = new ImageIcon(loader.getResource("tienda/malla2.png"));
+		lblMalla2.setIcon(imagenMalla2);
+
 		btnSalida.addActionListener(new ActionListener() {
-			
+
 			@Override
 			public void actionPerformed(ActionEvent e) {
 				dispose();
 			}
 		});
-		
+
 		btnEspada.addActionListener(new ActionListener() {
-			
+
 			@Override
 			public void actionPerformed(ActionEvent e) {
-				InterfazTienda v1 = new InterfazTienda();
-				v1.setVisible(true);
-			
+				Array.danyoarma = Array.danyoarma + 5;
+
+				JOptionPane.showMessageDialog(null, "Espada nivel 1 comprada!");
+
 			}
 		});
-		
+
 		btnEspada2.addActionListener(new ActionListener() {
-			
+
 			@Override
 			public void actionPerformed(ActionEvent e) {
-				// TODO Auto-generated method stub
-				InterfazTienda v2 = new InterfazTienda();
-				v2.setVisible(true);
-				
+				Array.danyoarma = Array.danyoarma + 12;
+				JOptionPane.showMessageDialog(null, "Espada nivel 2 comprada!");
+
 			}
 		});
-		
+
 		btnLanza.addActionListener(new ActionListener() {
-			
+
 			@Override
 			public void actionPerformed(ActionEvent e) {
-				// TODO Auto-generated method stub
-				InterfazTienda v3 = new InterfazTienda();
-				v3.setVisible(true);
-				
+				Array.energia = Array.energia + 6;
+				JOptionPane.showMessageDialog(null, "Lanza nivel 1 comprada!");
+
 			}
 		});
 		btnLanza2.addActionListener(new ActionListener() {
-			
+
 			@Override
 			public void actionPerformed(ActionEvent e) {
-				// TODO Auto-generated method stub
-				InterfazTienda v4 = new InterfazTienda();
-				v4.setVisible(true);
-				
+				Array.energia = Array.energia + 11;
+				JOptionPane.showMessageDialog(null, "Lanza nivel 2 comprada!");
 			}
 		});
 		btnMalla.addActionListener(new ActionListener() {
 			@Override
 			public void actionPerformed(ActionEvent e) {
-				// TODO Auto-generated method stub
-				InterfazTienda v5 = new InterfazTienda();
-				v5.setVisible(true);
-				
+				Array.health = Array.health + 10;
+				JOptionPane.showMessageDialog(null, "Malla nivel 1 comprada!");
+
 			}
 		});
 		btnMalla2.addActionListener(new ActionListener() {
 			@Override
 			public void actionPerformed(ActionEvent e) {
-				// TODO Auto-generated method stub
-				InterfazTienda v6 = new InterfazTienda();
-				v6.setVisible(true);
-				
+				Array.health = Array.health + 17;
+				JOptionPane.showMessageDialog(null, "Malla nivel 2 comprada!");
 			}
 		});
-		
+
 		btnPropEspada.addActionListener(new ActionListener() {
 			@Override
 			public void actionPerformed(ActionEvent e) {
 				// TODO Auto-generated method stub
-				InterfazTienda i1 = new InterfazTienda();
-				i1.setVisible(true);
-				JLabel lblStats = new JLabel("Daño: +5; Speed: +1");
-				lblStats.setFont(new Font("Times New Roman", Font.PLAIN, 25));
-				lblStats.setBounds(511, 466, 71, 29);
-				getContentPane().add(lblStats);
+				JOptionPane.showMessageDialog(null, "DaÃ±o: +5");
 			}
 		});
 		btnPropEspada2.addActionListener(new ActionListener() {
 			@Override
 			public void actionPerformed(ActionEvent e) {
 				// TODO Auto-generated method stub
-				InterfazTienda i2 = new InterfazTienda();
-				i2.setVisible(true);
-				JLabel lblStats = new JLabel("Daño: +12; Speed: +2");
-				lblStats.setFont(new Font("Times New Roman", Font.PLAIN, 25));
-				lblStats.setBounds(511, 466, 71, 29);
-				getContentPane().add(lblStats);
+				JOptionPane.showMessageDialog(null, "DaÃ±o: +12");
+
 			}
 		});
 		btnPropLanza.addActionListener(new ActionListener() {
 			@Override
 			public void actionPerformed(ActionEvent e) {
 				// TODO Auto-generated method stub
-				InterfazTienda i3 = new InterfazTienda();
-				i3.setVisible(true);
-				JLabel lblStats = new JLabel("Daño: +2; Speed: +3");
-				lblStats.setFont(new Font("Times New Roman", Font.PLAIN, 25));
-				lblStats.setBounds(511, 466, 71, 29);
-				getContentPane().add(lblStats);
+				JOptionPane.showMessageDialog(null, "Energia: +6");
+
 			}
 		});
 		btnPropLanza2.addActionListener(new ActionListener() {
 			@Override
 			public void actionPerformed(ActionEvent e) {
 				// TODO Auto-generated method stub
-				InterfazTienda i4 = new InterfazTienda();
-				i4.setVisible(true);
-				JLabel lblStats = new JLabel("Daño: +4; Speed: +5");
-				lblStats.setFont(new Font("Times New Roman", Font.PLAIN, 25));
-				lblStats.setBounds(511, 466, 71, 29);
-				getContentPane().add(lblStats);
+				JOptionPane.showMessageDialog(null, "Energia: +11");
+
 			}
 		});
 		btnPropMalla.addActionListener(new ActionListener() {
 			@Override
 			public void actionPerformed(ActionEvent e) {
 				// TODO Auto-generated method stub
-				InterfazTienda i5 = new InterfazTienda();
-				i5.setVisible(true);
-				JLabel lblStats = new JLabel("Speed: +7");
-				lblStats.setFont(new Font("Times New Roman", Font.PLAIN, 25));
-				lblStats.setBounds(511, 466, 71, 29);
-				getContentPane().add(lblStats);
+				JOptionPane.showMessageDialog(null, "Vida: +10");
 			}
 		});
 		btnPropMalla2.addActionListener(new ActionListener() {
 			@Override
 			public void actionPerformed(ActionEvent e) {
 				// TODO Auto-generated method stub
-				InterfazTienda i6 = new InterfazTienda();
-				i6.setVisible(true);
-				JLabel lblStats = new JLabel("Speed: +12");
-				lblStats.setFont(new Font("Times New Roman", Font.PLAIN, 25));
-				lblStats.setBounds(511, 466, 71, 29);
-				getContentPane().add(lblStats);
+				JOptionPane.showMessageDialog(null, "Vida: +17");
+
 			}
 		});
-		}
+	}
 }
+
