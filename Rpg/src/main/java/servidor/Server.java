dd1f50bf0631b2ad542930deeeb7c1380074368a
xabie <xabie@192.168.1.35>
2021-05-18 21:35:20 +0200

    http

--- Rpg/src/main/java/servidor/Server.java
+++ Rpg/src/main/java/servidor/Server.java
@@ -9,6 +9,9 @@ import java.net.URI;
  * Server class.
  *
  */
+
+//http://localhost:8080/myapp/myresource
+
 public class Server {
 	  // Base URI the Grizzly HTTP server will listen on
     public static final String BASE_URI = "http://localhost:8080/myapp/";
