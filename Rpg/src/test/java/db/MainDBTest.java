7f535b43707ab11ffc824c23e56442d868fbf6a6
Iker <iker.tobal@opendeusto.es>
2021-05-22 17:27:46 +0200

    completados los test unitarios para bd

--- Rpg/src/test/java/db/MainDBTest.java
+++ Rpg/src/test/java/db/MainDBTest.java
@@ -18,15 +18,44 @@ import dinero.Monedero;
 import org.junit.*;
 import org.junit.Before;
 import org.junit.Test;
+import org.junit.runner.RunWith;
 import org.mockito.Mock;
 
 public class MainDBTest {
 	
+	@Mock
 	private static MainDB db = new MainDB();
 	private static Partida partida = new Partida();
 	private static Partida partida2 = new Partida();
 	private static Estadisticas e1 = new Estadisticas();
 	private static Estadisticas e2 = new Estadisticas();
+	
+	
+	@Before
+	public void setUp2() {
+		Monedero m = new Monedero();
+		partida.setNombrePartida("Example");
+		partida.setSkin("Ezio, el Arquero Centenario");
+		partida.setVida(100);
+		partida.setX(8);
+		partida.setY(8);
+		partida.setX_dib(3);
+		partida.setY_dib(3);
+		partida.setMonedero(m);
+		partida.setEnergia(100);
+		partida.setDanyoarma(50);
+		
+		partida2.setNombrePartida("Example2");
+		partida2.setSkin("Ezio, el Arquero Centenario");
+		partida2.setVida(100);
+		partida2.setX(8);
+		partida2.setY(8);
+		partida2.setX_dib(3);
+		partida2.setY_dib(3);
+		partida2.setMonedero(m);
+		partida2.setEnergia(100);
+		partida2.setDanyoarma(50);
+	}
 	
 	@BeforeClass
 	public static void setUp() throws Exception {
@@ -69,32 +98,6 @@ public class MainDBTest {
 		e2.setSkin("Ezio, el Arquero Centenario");
 		e2.setVida(100);
 	}
-	
-	@Before
-	public void setUp2() {
-		Monedero m = new Monedero();
-		partida.setNombrePartida("Example");
-		partida.setSkin("Ezio, el Arquero Centenario");
-		partida.setVida(100);
-		partida.setX(8);
-		partida.setY(8);
-		partida.setX_dib(3);
-		partida.setY_dib(3);
-		partida.setMonedero(m);
-		partida.setEnergia(100);
-		partida.setDanyoarma(50);
-		
-		partida2.setNombrePartida("Example2");
-		partida2.setSkin("Ezio, el Arquero Centenario");
-		partida2.setVida(100);
-		partida2.setX(8);
-		partida2.setY(8);
-		partida2.setX_dib(3);
-		partida2.setY_dib(3);
-		partida2.setMonedero(m);
-		partida2.setEnergia(100);
-		partida2.setDanyoarma(50);
-	}
 
 	@Test
 	public void testGuardarPartida() {
@@ -104,8 +107,16 @@ public class MainDBTest {
 	}
 	
 	@Test
+	public void testGuardarEstadisticas() {
+		boolean res = false; 
+		res = db.guardarEstadisticas(e2);
+		assertTrue(res);
+	}
+	
+	@Test
 	public void testCargarPartida() {
 		String[] p = new String[9];
+		db.guardarPartida(partida);
 		p = db.cargarPartida("Example");
 		assertEquals("Ezio, el Arquero Centenario", p[0]);
 		assertEquals("8", p[1]);
@@ -119,22 +130,25 @@ public class MainDBTest {
 		
 	}
 	
-//	@Test
-//	public void testMostrarPartidas() {
-//		System.out.println(partida.toString());
-//		System.out.println(partida2.toString());
-//		List<Partida> lista = new ArrayList<Partida>();
-//		lista.add(partida);
-//		lista.add(partida2);
-//		
-//		assertEquals(lista, db.mostrarPartidas());
-//	}
+	@Test
+	public void testMostrarPartidas() {
+		db.guardarPartida(partida);
+		db.guardarPartida(partida2);
+		List<Partida> listaExpected = new ArrayList<Partida>();
+		listaExpected.add(partida);
+		listaExpected.add(partida2);
+		
+		assertEquals(listaExpected.get(0).toString(), db.mostrarPartidas().get(0).toString());
+	}
 	
 	@Test
-	public void testGuardarEstadisticas() {
-		boolean res = false; 
-		res = db.guardarEstadisticas(e2);
-		assertTrue(res);
+	public void testMostrarEstadisticas() {
+		db.guardarEstadisticas(e2);
+		List<Estadisticas> listaExpected = new ArrayList<Estadisticas>();
+		listaExpected.add(e1);
+		listaExpected.add(e2);
+		
+		assertEquals(listaExpected.get(0).toString(), db.mostrarEstadisticas().get(0).toString());
 	}
 	
 	@After
--- Rpg/src/test/java/db/PartidaTest.java
+++ Rpg/src/test/java/db/PartidaTest.java
@@ -44,5 +44,28 @@ public class PartidaTest {
 	public void testConstructor() {
 		assertEquals(prueba2.toString(), prueba.toString());
 	}
+	@Test
+	public void testSetters() {
+		prueba.setMonedero(m);
+		prueba.setNombrePartida("PARTIDA");
+		prueba.setSkin("caballero");
+		prueba.setVida(100);
+		prueba.setX(2);
+		prueba.setY(2);
+		prueba.setX_dib(3);
+		prueba.setY_dib(3);
+		prueba.setDanyoarma(50);
+		prueba.setEnergia(100);
+		assertEquals(prueba.getNombrePartida(),"PARTIDA");
+		assertEquals(prueba.getMonedero(), m);
+		assertEquals(prueba.getSkin(), "caballero");
+		assertEquals(prueba.getVida(), 100);
+		assertEquals(prueba.getX(), 2);
+		assertEquals(prueba.getY(), 2);
+		assertEquals(prueba.getX_dib(), 3);
+		assertEquals(prueba.getY_dib(), 3);
+		assertEquals(prueba.getDanyoarma(), 50);
+		assertEquals(prueba.getEnergia(), 100);
+	}
 
 }
