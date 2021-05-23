7560d28f2dc99133849f6b1a8dc40084a5527cc3
David <33113652+DavidLopezLoureiro@users.noreply.github.com>
2021-05-23 18:54:45 +0200

    Comentarios

--- Rpg/src/main/java/mapa/Array.java
+++ Rpg/src/main/java/mapa/Array.java
@@ -62,12 +62,18 @@ public class Array extends JFrame implements KeyListener {
 	static String direccion = "RIGHT";
 	private static String skin = "";
 	boolean in = false;
+	
+	/** Aumenta el daño del arma*/
 	public static void mejora() {
 		danyoarma +=10;
 	}
+	
+	/**Aumenta la vida*/
 	public static void mejoraVida() {
 		health +=10;
 	}
+	
+	/** Aumenta el mana*/
 	public static void mejorarMana() {
 		energia +=10;
 	}
@@ -80,58 +86,92 @@ public class Array extends JFrame implements KeyListener {
 
 	public static JPanel contentPane;
 
-	
+	/** Obtiene la vida*/
 	public int getVida() {
 		return vida;
 	}
+	
+	/** Pone la vida a un número exacto*/
 	public void setVida(int vida) {
 		this.vida = vida;
 	}
+	
+	/** Obtiene la X*/
 	public int getX() {
 		return x;
 	}
+	
+	/** Cambia la X*/
 	public static void setX(int x) {
 		Array.x = x;
 	}
+	
+	/** Obtiene la Y*/
 	public int getY() {
 		return y;
 	}
+	
+	/** Cambia la Y*/
 	public static void setY(int y) {
 		Array.y = y;
 	}
+	
+	/** Obtiene la X del dibujado del mapa*/
 	public static int getX_dib() {
 		return x_dib;
 	}
+	
+	/** Cambia la X del dibujado del mapa*/
 	public static void setX_dib(int x_dib) {
 		Array.x_dib = x_dib;
 	}
+	
+	/** Obtiene la Y del diujado del mapa*/
 	public static int getY_dib() {
 		return y_dib;
 	}
+	
+	/** Cambia la Y del dibujado del mapa*/
 	public static void setY_dib(int y_dib) {
 		Array.y_dib = y_dib;
 	}
+	
+	/** Obtiene el oro de la cartera*/
 	public static Monedero getCartera() {
 		return cartera;
 	}
+	
+	/** Cambia la el dinero de la cartera*/
 	public static void setCartera(Monedero cartera) {
 		Array.cartera = cartera;
 	}
+	
+	/** Obtiene la skin del personaje*/
 	public static String getSkin() {
 		return skin;
 	}
+	
+	/** Cambia la skin del personaje*/
 	public void setSkin(String skin) {
 		Array.skin = skin;
 	}
+	
+	/** Obtiene el daño del arma*/
 	public static int getDanyoarma() {
 		return danyoarma;
 	}
+	
+	/** Cambia el daño del arma*/
 	public static void setDanyoarma(int danyoarma) {
 		Array.danyoarma = danyoarma;
 	}
+	
+	/** Obtiene la energia*/
 	public static int getEnergia() {
 		return energia;
 	}
+	
+	/** Cambia la energia*/
 	public static void setEnergia(int energia) {
 		Array.energia = energia;
 	}
@@ -149,6 +189,7 @@ public class Array extends JFrame implements KeyListener {
 		});
 	}
 	
+	/** Se usa para seleccionar la musica*/
 	public void musica() {
 		
 		try {
@@ -211,6 +252,7 @@ public class Array extends JFrame implements KeyListener {
 
 	}
 	
+	/** Inicia el juego principal*/
 	public Array() {
 		
 		musica();
@@ -2732,7 +2774,7 @@ public class Array extends JFrame implements KeyListener {
 		dibujado();
 
 	}
-	// este metodo sirve para cambiar el mapa del combate en base a la zona en la que estes en el mapa principal
+	/**Este metodo sirve para cambiar el mapa del combate en base a la zona en la que estes en el mapa principal*/
 	public void cambioZona() {
 		if(zona.equals("grass")) {Room.zona=1;}
 		if(zona.equals("sand")) {Room.zona=2;}
@@ -2740,7 +2782,7 @@ public class Array extends JFrame implements KeyListener {
 		if(zona.equals("fire")||zona.equals("dungeon")) {Room.zona=4;}
 		
 	}
-
+	/** Dibuja el mapa del mundo*/
 	public void dibujado() {
 		
 		if (mundo[x][y].getCode() == ("chest")) {
@@ -3268,7 +3310,7 @@ public class Array extends JFrame implements KeyListener {
 
 	}
 
-	
+	/** Se usa para la carga de la mazmorra*/
 	public void cargar() {
 
 		try {
@@ -3328,6 +3370,8 @@ public class Array extends JFrame implements KeyListener {
 		repaint();
 
 	}
+	
+	/** Lector de teclas*/
 	public void keyPressed(KeyEvent e) {
 
 		int key = e.getKeyCode();
@@ -3435,11 +3479,12 @@ public class Array extends JFrame implements KeyListener {
 		}
 
 	}
-
+	/** Cuando se suelta la tecla*/
 	public void keyReleased(KeyEvent arg0) {
 
 	}
-
+	
+	/** Cuando se pulsa la tecla*/
 	public void keyTyped(KeyEvent arg0) {
 
 	}
