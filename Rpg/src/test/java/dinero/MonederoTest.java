b983632ce22eafacfcdc77ab73043f14f16d897c
David <33113652+DavidLopezLoureiro@users.noreply.github.com>
2021-05-27 17:50:51 +0200

    arreglos

--- Rpg/src/test/java/dinero/MonederoTest.java
+++ Rpg/src/test/java/dinero/MonederoTest.java
@@ -1,3 +1,24 @@
+package dinero;
+
+import static org.junit.Assert.*;
+import static org.mockito.Mockito.when;
+
+import org.junit.Test;
+import org.junit.experimental.categories.Category;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+
+import dinero.Monedero;
+
+import junit.framework.JUnit4TestAdapter;
+
+import org.junit.Before;
+import org.junit.Rule;
+import org.databene.contiperf.Required;
+import org.databene.contiperf.PerfTest;
+import org.databene.contiperf.junit.ContiPerfRule;
+import org.databene.contiperf.report.EmptyReportModule;
+import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import categories.IntegrationTest;
@@ -5,14 +26,15 @@ import categories.IntegrationTest;
 
 public class MonederoTest {
 
-
+	
 	Monedero moneda;
-
-
-
-
+	
+	
+	
+	
 	@Rule
 	public ContiPerfRule rule = new ContiPerfRule();
+
 	
 	
 	
@@ -20,7 +42,7 @@ public class MonederoTest {
 	@PerfTest(invocations = 150)
 	@Required(max = 20, average = 1)
 	public void RendiTestMoneda() {
-
+		
 		moneda = new Monedero();
 		assertTrue(moneda.getDinero() == 1000);
 
@@ -45,7 +67,7 @@ public class MonederoTest {
 		assertTrue(moneda.getDinero() == 900);
 
 	}
-
+	
 	@Test
 	@PerfTest(invocations = 200)
 	@Required(max = 20, average = 1)
@@ -53,7 +75,8 @@ public class MonederoTest {
 		moneda = new Monedero();
 		moneda.compra(100);
 		assertTrue(moneda.getDinero() == 900);	
-
-
+		
+		
 	}
+
 }
